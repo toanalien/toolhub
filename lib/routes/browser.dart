58c393b5a5f51db9f8fba158e6976d9d8c6c001f
luojie27 <luojie27@meituan.com>
2022-09-21 14:05:26 +0800

    feat: add web3 provider

--- lib/routes/browser.dart
+++ lib/routes/browser.dart
@@ -1,18 +1,105 @@
+import 'dart:collection';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:toolhub/theme.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'dart:io';
 import 'package:wallet_connect/wallet_connect.dart';
+import 'package:web3_provider/js_bridge_callback_bean.dart';
+import 'package:web3_provider/json_util.dart';
+
+mixin Web3ProviderMixin<T extends StatefulWidget> on State<T> {
+  final _alertTitle = "messagePayTube";
+
+  String? jsProviderScript;
+
+  /// Function initial web3
+  String? functionInject;
+
+  /// Load provider and function initial web3 end
+  bool isLoadJs = true;
+
+  ///Load provider and function initial web3 to inject web app
+  Future<void> _loadWeb3() async {
+    String? web3;
+    String? walletName = 'trustwallet';
+    String path = 'packages/web3_provider/assets/trust-min.js';
+    web3 = await DefaultAssetBundle.of(context).loadString(path);
+    int chainId = 1;
+    String rpcUrl = "";
+    String walletAddress = "";
+    bool isDebug = true;
+    var config = """
+         (function() {
+           var config = {
+                chainId: $chainId,
+                rpcUrl: "$rpcUrl",
+                address: "$walletAddress",
+                isDebug: $isDebug
+            };
+            window.isWeb3chat = true;
+            window.ethereum = new $walletName.Provider(config);
+            window.web3 = new $walletName.Web3(window.ethereum);
+            $walletName.postMessage = (jsonString) => {
+               alert("$_alertTitle" + JSON.stringify(jsonString || "{}"))
+            };
+
+            console.log("ok: ", ethereum, web3);
+        })();
+        """;
+    if (mounted) {
+      setState(() {
+        jsProviderScript = web3;
+        functionInject = config;
+        isLoadJs = true;
+      });
+    }
+  }
+
+  /// Callback handle data receive from dapp
+  Future<void> _jsBridgeCallBack(
+      String message, InAppWebViewController controller) async {
+    Map<dynamic, dynamic> params = JsonUtil.getObj(message);
+    final name = params["name"];
+    final id = params["id"];
+    log('bridge call: $name, $params');
+
+    // BridgeParams
+    switch (name) {
+      case 'requestAccounts':
+      case 'eth_requestAccounts':
+        // widget.signCallback(rawData, EIP1193.requestAccounts, _webViewController);
+        break;
+      case 'signTransaction':
+        break;
+      case 'signMessage':
+        break;
+      case 'signPersonalMessage':
+        break;
+      case 'signTypedMessage':
+        break;
+      case 'request':
+        break;
+      default:
+    }
+
+    controller.cancel(id);
+  }
+}
 
 class Browser extends StatefulWidget {
   final String url;
-  const Browser({Key? key, required this.url}) : super(key: key);
+  const Browser({Key? key, required this.url, this.initialUserScripts})
+      : super(key: key);
+
+  final UnmodifiableListView<UserScript>? initialUserScripts;
 
   @override
   State<Browser> createState() => _BrowserState();
 }
 
-class _BrowserState extends State<Browser> {
+class _BrowserState extends State<Browser> with Web3ProviderMixin {
   final GlobalKey webViewKey = GlobalKey();
 
   String title = "";
@@ -64,6 +151,7 @@ class _BrowserState extends State<Browser> {
 
   @override
   void initState() {
+    _loadWeb3();
     super.initState();
 
     setState(() {
@@ -91,8 +179,38 @@ class _BrowserState extends State<Browser> {
       initialUrlRequest: URLRequest(
         url: _uri,
       ),
+      initialUserScripts: (isLoadJs == true
+          ? UnmodifiableListView([
+              UserScript(
+                source: jsProviderScript ?? '',
+                injectionTime: UserScriptInjectionTime.AT_DOCUMENT_START,
+              ),
+              UserScript(
+                source: functionInject ?? '',
+                injectionTime: UserScriptInjectionTime.AT_DOCUMENT_START,
+              ),
+            ])
+          : null),
       onWebViewCreated: (controller) {
         print('created');
+      },
+      onJsAlert: (controller, request) {
+        final message = request.message;
+        bool handledByClient = false;
+        if (message?.contains(_alertTitle) == true) {
+          handledByClient = true;
+          _jsBridgeCallBack(
+            request.message!.replaceFirst(_alertTitle, ""),
+            controller,
+          );
+        }
+
+        return Future.value(
+          JsAlertResponse(
+            message: message ?? "",
+            handledByClient: handledByClient,
+          ),
+        );
       },
       pullToRefreshController: pullToRefreshController,
       onLoadStop: (controller, url) async {
@@ -103,6 +221,16 @@ class _BrowserState extends State<Browser> {
             title = value ?? "";
           });
         });
+      },
+      onLoadStart: (controller, url) async {
+        // if (Platform.isAndroid) {
+        await controller.evaluateJavascript(
+          source: jsProviderScript ?? '',
+        );
+        await controller.evaluateJavascript(
+          source: functionInject ?? '',
+        );
+        // }
       },
       onProgressChanged: (controller, progress) {
         if (progress == 100) {
--- lib/routes/home/home.dart
+++ lib/routes/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toolhub/navigator.dart';
+import 'package:toolhub/routes/browser.dart';
 import 'package:toolhub/routes/crypto/home.dart';
 
 class HomePage extends StatefulWidget {
@@ -25,6 +26,20 @@ class _HomePageState extends State<HomePage> {
             child: IconButton(
               onPressed: () {
                 MNavigator.push(context, (ctx, arguments) => const Pocket());
+              },
+              icon: Icon(Icons.wallet),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: IconButton(
+              onPressed: () {
+                MNavigator.push(
+                  context,
+                  (ctx, arguments) => Browser(
+                    url: 'http://127.0.0.1:8000',
+                  ),
+                );
               },
               icon: Icon(Icons.wallet),
             ),
